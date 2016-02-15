b5fc767ffe6abe6482e8ab7594dbad48e908c366
J. Drill <JJDrill@hotmail.com>
2016-02-15 12:37:27 -0700

    Removed email and password checking for now.

--- app/src/main/java/com/didgeridone/didgeridone_andriod/LoginActivity.java
+++ app/src/main/java/com/didgeridone/didgeridone_andriod/LoginActivity.java
@@ -193,12 +193,14 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
     private boolean isEmailValid(String email) {
         //TODO: Replace this with your own logic
-        return email.contains("@");
+        //return email.contains("@");
+        return true;
     }
 
     private boolean isPasswordValid(String password) {
         //TODO: Replace this with your own logic
-        return password.length() > 4;
+        //return password.length() > 4;
+        return true;
     }
 
     /**
